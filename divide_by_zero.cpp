685b13e6a374eda5ea87faa19139b6a34f1b48c4
jadTalbert <jad.talbert@gmail.com>
2025-08-27 12:15:26 -0400

    Lab 1 product and divide programs

---
+++ divide_by_zero.cpp
@@ -0,0 +1,23 @@
+//@author: Francisco Santiago
+//date: 8/27/25
+//@purpose: Divide a number by zero and review error.
+
+//include the iostream directive
+#include <iostream>
+
+//use the standard namespace
+using namespace std;
+
+//define the main() function/method
+int main()
+{
+    int num1 = 1-10;
+    int num2 = 0;
+    int quotient = num1 / num2;
+
+    //use the standard output for the console and print the quotient of the variables defined above
+    std::cout << quotient << std::endl;
+
+    return 0;
+}
+
---
+++ product_of_two_numbers.cpp
@@ -0,0 +1,23 @@
+//@author: Francisco Santiago
+//date: 8/27/25
+//@purpose: Calculate the product of two numbers and print result to the console.
+
+//include the iostream directive
+#include <iostream>
+
+//use the standard namespace
+using namespace std;
+
+//define the main() function/method
+int main()
+{
+    int num1 = 5;
+    int num2 = 10;
+    int product = num1 * num2;
+
+    //use the std output for the console and print the product of the variables defined above
+    std::cout << product << std::endl;
+
+    return 0;
+
+}
