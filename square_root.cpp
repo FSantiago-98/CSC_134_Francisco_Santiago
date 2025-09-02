0d32458d44448dca1093f0250da5338a1480be82
FSantiago-98 <santiagofj622@mail.sandhills.edu>
2025-09-02 16:16:17 -0400

    Lab 1 square_root.cpp

---
+++ square_root.cpp
@@ -0,0 +1,20 @@
+//@author: Francisco Santiago
+//@date: 9/2/25
+//@purpose: find the square root of a real number
+
+//include the iostream directive
+#include <iostream>
+#include <cmath>
+
+//use the standard namespace
+using namespace std;
+
+//define the main() function
+int main()
+{
+  int num1 = 4;
+  int root = sqrt(num1);
+
+  cout << root << endl;
+
+  return 0;
