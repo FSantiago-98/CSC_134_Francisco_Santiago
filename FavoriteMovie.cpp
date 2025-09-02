2a12d98ab5dbfe32363be386529140f50d670f3f
FSantiago-98 <santiagofj622@mail.sandhills.edu>
2025-09-02 15:54:36 -0400

    Lab 1 FavoriteMovie.cpp

---
+++ FavoriteMovie.cpp
@@ -0,0 +1,25 @@
+//@author: Francisco Santiago
+//@date: 9/2/25
+//@purpose: Print my favorite movie and the main characters to the console
+
+//include iostream and string directive
+#include <iostream>
+#include <string>
+
+//use standard namespace
+using namespace standard;
+
+//define main() function
+int main()
+{
+  string myFavotireMovie = "The Incredibles";
+  string mainCharacter1 = "Mr Incredible";
+  string mainCharacter2 = "ElastiGirl";
+  string mainCharacter3 = "Dash";
+  string mainCharacter4 = "Violet";
+
+  cout << "My favorite movie is "<<  myFavoriteMovie << endl;
+  cout << "The main characters are " << mainCharacter1 <<  mainCharacter2 <<  mainCharacter3 << " and" <<  mainCharacter4 << endl;
+
+  return 0;
+}
