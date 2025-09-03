9a89e0b9971e1c53945a538fbfd335d8bb65cb3f
SANDHILLS\LH218-08 <jad.talbert@gmail.com>
2025-09-03 12:21:00 -0400

    Lab 3

---
+++ Lab3/average_temp.cpp
@@ -0,0 +1,30 @@
+//@author: Francisco Santiago
+//@date: 9/3/25
+//@purpose: print the new average temp to the console after a two percent increase
+
+//include iostream directive
+#include <iostream>
+
+//use the standard namespace
+using namespace std;
+
+//define the main function
+int main()
+
+{ 
+    //use int and float variables for temps, the increase, and new temp results
+    int nycTempjuly = 85;
+    int denTempjuly = 88;
+    int phoeTempjuly = 106;
+    float tempInc = 0.02;
+    float newNYCtemp = nycTempjuly * tempInc;
+    float newDentemp = denTempjuly * tempInc;
+    float newPhoetemp = phoeTempjuly * tempInc;
+
+    //print the new temps to the console
+    cout << "The new average July temperature for NYC is: " << newNYCtemp << endl;
+    cout << "For Denver: " << newDentemp << endl;
+    cout << "For Phoenix: " << newPhoetemp << endl;
+
+    return 0;
+}
