5eb5168bdd23ceca49df2fddbab4d23856fd78de
FSantiago-98 <santiagofj622@mail.sandhills.edu>
2025-09-17 12:20:56 -0400

    Create infinite_loop.cpp

---
+++ infinite_loop.cpp
@@ -0,0 +1,18 @@
+//@author: Francisco Santiago
+//@DATE: 9/17/25
+//@purpose: create an infinite loop using the while loop
+
+//include the iostream directive
+#include <iostream>
+
+//use the namespace standard
+using namespace std;
+
+//define the main function
+int main()
+
+while (true){
+    cout << "This is an infinite loop!" << endl;
+
+    return 0;
+}
