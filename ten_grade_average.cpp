d84924216c192ca6ccc25b170daa3749526f0046
FSantiago-98 <santiagofj622@mail.sandhills.edu>
2025-09-17 12:21:58 -0400

    Create ten_grade_average.cpp

---
+++ ten_grade_average.cpp
@@ -0,0 +1,23 @@
+//@author: Francisco Santiago
+//@date: 9/17/25
+//@purpose: use a while loop to gather ten inputs and output the average
+
+//include iostream directive
+#include <iostream>
+
+//use the standard namespace
+using namespace std;
+
+//define the main function
+int main()
+{
+    int tenGrades = 0
+    float gradeInc
+    float 
+    float avg
+
+    while(tenGrades < 10){
+        
+    }
+
+}
