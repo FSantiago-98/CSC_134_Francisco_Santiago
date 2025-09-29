813e7b24752f8d40a2c37ff7d00c67523dadc202
FSantiago-98 <santiagofj622@mail.sandhills.edu>
2025-09-29 11:26:24 -0400

    Create count_backwards.cpp

---
+++ Lab6/count_backwards.cpp
@@ -0,0 +1,21 @@
+//@author: Francisco Santiago
+//@date: 9/28/25
+//@purpose: use a for loop to count backwards from 100
+
+//include the iostream directive
+#include <iostream>
+
+//use the namespace standard
+using namespace std
+
+//define the main function
+int main()
+{
+int num = 100  //initialize an integer variable of 100
+  //use a for loop to count back from 100 and print out the num every iteration
+  for (num = 100; num > 1; num--){
+cout << num << endl;}
+
+return 0;
+
+}
