f33608a8b01f621811a39acc19544ec619449ce7
FSantiago-98 <santiagofj622@mail.sandhills.edu>
2025-09-29 11:58:16 -0400

    Create accumulator_count.cpp

---
+++ Lab6/accumulator_count.cpp
@@ -0,0 +1,27 @@
+//@author: Francisco Santiago
+//@date:9/28/25
+//@purpose: use a for loop to iterate 100 times and add one to an accumulator for every loop
+
+//include the iostream directive
+#include <iostream>
+
+//use the standard namespace
+using namespace std;
+
+//define the main function
+int main()
+{
+//initialize two integer variables with o
+int accumulator = 0;
+int num = 0;
+//use a for loop to iterate from 0 to 100 and add 1 to the accumulator for every loop
+for (num = 0; num < 100; num++){
+cout << num << endl;
+accumulator += 1;
+}
+
+cout << "The accumulator value is: " << accumulator << endl;
+
+return 0;
+
+}
