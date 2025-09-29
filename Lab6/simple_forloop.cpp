70cf9ff5a56b97166d60589f5e654ac35e055cd9
FSantiago-98 <santiagofj622@mail.sandhills.edu>
2025-09-29 11:10:39 -0400

    Update and rename somple_forloop.cpp to simple_forloop.cpp

---
+++ Lab6/simple_forloop.cpp
@@ -0,0 +1,21 @@
+//@author: Francisco Santiago
+//@date: 9/29/25
+//@purpose: use  simple for loop that starts at 0 and ends at 10, also prints out the count after each iteration
+
+//include the iosream directive
+#include <iostream>
+
+//use the namespace standard
+using namespace std
+
+//define the main function
+int main()
+{
+  int num = 0
+for (num = 0; num < 10; num++){
+cout << num << endl;
+}
+  
+return 0;
+  
+}
