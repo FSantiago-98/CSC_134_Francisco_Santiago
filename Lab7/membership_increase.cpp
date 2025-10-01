bf03a6b2ee9a86e81ec132bdb5faa7e42930edda
FSantiago-98 <santiagofj622@mail.sandhills.edu>
2025-10-01 11:15:39 -0400

    Create membership_increase.cpp

---
+++ Lab7/membership_increase.cpp
@@ -0,0 +1,20 @@
+//@author: Francisco Santiago
+//@date: 10/1/25
+//@purpose: use for loops to display increase in sales for a country club over six years
+
+//include iostream directive
+#include <iostream>
+
+//use the standard namespace
+using namespace std;
+
+//define the main funstion
+int main()
+{
+  double membership = 250,000; //variable that holds the current cost of membership
+  double percentInc = 0.06; //variable that holds the percentage increase 
+  double newYearlyprice;
+  int year;
+
+  
+  
