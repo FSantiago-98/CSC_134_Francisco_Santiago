d2699eee768677f9eddd7a14baa29abed1f2c5b1
FSantiago-98 <santiagofj622@mail.sandhills.edu>
2025-10-01 20:36:52 -0400

    Update random_dice_roll.cpp

--- Lab6/random_dice_roll.cpp
+++ Lab6/random_dice_roll.cpp
@@ -2,7 +2,7 @@
 //@date:9/28/25
 //@purpose: use a for loop to simulate a dice roll
 
-//include iostream and cstdlib directive
+//include iostream, ctime, and cstdlib directive
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
@@ -13,14 +13,14 @@ using namespace std;
 //define the main function
 int main()
 {
-    int diceRoll;
-    int roll;
+    int diceRoll; //variable for user input
+    int roll; //variable to generate random number in for loop
     
     //use cout and cin to ask user for amount of times to roll dice
     cout << "How many times do you want to roll the dice?" << endl;
     cin >> diceRoll;
 
-    srand(time(0));
+    srand(time(0)); //seed the random generator
 
     //use for loop to roll dice 
     for (int i = 1; i <= diceRoll; i++){
