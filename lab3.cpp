0cd3e7c2606943d9280f9f71b561fb7092951aaf
FSantiago-98 <santiagofj622@mail.sandhills.edu>
2025-10-29 12:18:54 -0400

    Create lab3.cpp for lab 10
    Initialize lab3.cpp with basic structure and comments.

---
+++ lab3.cpp
@@ -0,0 +1,18 @@
+//@author: Francisco Santiago
+//@date: 10/29/25
+//@purpose: Input sales from a file and create an array to find total of sales
+
+//include directives
+#include <iostream>
+#include <fstream>
+
+//use the namespace standard
+using namespace std;
+
+//define the main function
+int main()
+{
+    //define the variables
+}
+    
+
