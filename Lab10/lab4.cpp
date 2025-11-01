0bc68b0172fe8917ac0ecf88fa029687dd8b11f0
FSantiago <santiagofj622@mail.sandhills.edu>
2025-11-01 19:59:49 -0400

    lab10

---
+++ Lab10/lab4.cpp
@@ -0,0 +1,18 @@
+//@author: Francisco Santiago
+//@date: 11/1/25
+//@purpose: use luhn algorithm to check validity of a credit card number
+
+//include directives
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <algorithm>
+
+//use the namespace std
+using namespace std;
+
+//define the main function
+int main()
+{
+    
+}
