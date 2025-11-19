cac17fe6fbd4cc4037da7c8af8a032464a8314cc
FSantiago-98 <santiagofj622@mail.sandhills.edu>
2025-11-19 11:20:16 -0500

    Starting week 14 labs
    Implement a function to display a welcome message in the console.

---
+++ display_welcome.cpp
@@ -0,0 +1,25 @@
+//@author: Francisco Santiago 
+//@date: 11/19/25
+//@purpose: use a function to display a message to the console
+
+//include directives
+#incude <iostream>
+
+//use namespace standard
+using namespace std;
+
+//define a function
+void displayWelcome(){
+  //display strings to the console
+  cout << "==============================\n";
+  cout << "C++ Function Demonstration\n";
+  cout << "==============================\n";
+  return;
+  }
+
+//define the main function
+int main(){
+  cout << "Im in main.\n"; //Prints out a string that clarifies we are in the main function
+  displayWelcome() //calls the displayWelcome function and displays the strings in it
+cout << "Im back in main>\n"; //clarifies we are back in main after calling displayWelcome
+return 0;
