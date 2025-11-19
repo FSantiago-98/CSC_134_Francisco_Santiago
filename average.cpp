f971901ab1db001f795de9eb1af77c502088330d
FSantiago-98 <santiagofj622@mail.sandhills.edu>
2025-11-19 11:58:00 -0500

    lab 3 for week 14
    Implement a function to calculate the average of three values and display the result.

---
+++ average.cpp
@@ -0,0 +1,35 @@
+//@author: Francisco Santiago
+//@date:11/19/25
+//@purpose: use functions to calculate an average
+
+//include directives
+#include <iostream>
+
+//use the namespace standard
+using namespace std;
+
+//define the function to calculate average
+double averageValues(double int1, double int2, double int3){
+  return (int1 + int2 + int3) / 3;
+}
+
+//define the main function
+int main(){
+  //declare variables
+  double val1 = 0;
+  double val2 = 0;
+  double val3 = 0;
+  double total = 0;
+
+  //prompt user to enter three values
+  cout << "Please enter three values to calculate the average: ";
+  cin >> val1 >> val2 >> val3;
+
+  //calculate average using the averageValues function
+  total = averageValues(val1, val2, val3)
+
+  //display the average
+  cout << "The average is " << total << endl;
+  return 0;
+}
+  
