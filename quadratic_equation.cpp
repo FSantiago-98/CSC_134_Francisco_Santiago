f7e0bd73ea827306b8eb38196f7f98d9e9338843
FSantiago-98 <santiagofj622@mail.sandhills.edu>
2025-12-08 11:55:01 -0500

    finishing last lab
    Implement quadratic equation solver using the quadratic formula.

---
+++ quadratic_equation.cpp
@@ -0,0 +1,20 @@
+//@author: Francisco Santiago
+//@date: 12/8/25
+//@purpose: use the quadratic formula to determine a distance
+
+//include directives
+#include <iostream>
+#include <cmath>
+
+//use the namespace standard
+using namespace std;
+
+//define the main function
+int main()
+{
+  //declare variables
+  double a = -0.01568
+  double b = 1.00000
+  double c = 1.5
+  double x = (-b - sqrt(pow(b, 2) - (4 * a * c))) / (2 * a)
+  
